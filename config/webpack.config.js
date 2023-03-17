594491feb3f87e9d9340a66e10979472af73a5d9
Pavel <ahalai365@mail.ru>
2023-03-17 17:23:11 +0500

    Накидал мок

---
+++ config/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "./../dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    static: path.resolve(__dirname, "./../dist"),
+    open: true,
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
